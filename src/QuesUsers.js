28fcf86ac6e87c89716b65911a39ab2fd55d6481
Arboker <denislupookov@gmail.com>
2020-12-07 22:57:17 +0200

    horizontal

--- src/QuesUsers.js
+++ src/QuesUsers.js
@@ -111,8 +111,8 @@ class QuesUsers extends React.Component {
 
                 <Group style={{ paddingBottom: 8 }}>
                     {this.state.loaded ? (
-                        <HorizontalScroll style={{padding: "0 10px"}}>
-                            <div style={{ display: 'flex' }}>
+                        <HorizontalScroll>
+                            <div style={{ display: 'flex', padding: "0 10px" }}>
                                 {this.state.allUsers.map(user => {
                                     return (
                                         <div style={{ ...itemStyle }}
